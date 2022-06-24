3af7be7f0577d72cfdd70248eccd7ae3da23a78b
ugocottin <ugo.cottin@gmail.com>
2022-06-24 16:48:29 +0200

    stable

--- Package.swift
+++ Package.swift
@@ -1,22 +1,23 @@
-// swift-tools-version:5.5
-// The swift-tools-version declares the minimum version of Swift required to build this package.
+// swift-tools-version:5.0.0
 
 import PackageDescription
 
 let package = Package(
     name: "CFirebird",
 	platforms: [
-		.macOS(.v10_10)
+		.macOS(.v10_10),
 	],
     products: [
         .library(
-            name: "CFirebird",
-            targets: ["CFirebird"]),
+			name: "CFirebird",
+			targets: ["CFirebird"]),
     ],
     targets: [
-		.systemLibrary(name: "CFirebird", pkgConfig: "libfbclient"),
+		.systemLibrary(
+			name: "CFirebird",
+			pkgConfig: "libfbclient"),
         .testTarget(
-            name: "CFirebirdTests",
-            dependencies: ["CFirebird"]),
+			name: "CFirebirdTests",
+			dependencies: ["CFirebird"]),
     ]
 )
